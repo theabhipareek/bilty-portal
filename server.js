9ff03d2b647662fb5b7f9243c2375215ebbfc98e
Abhi Pareek <abhi@edxtra.online>
2025-01-27 13:25:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const PDFDocument = require('pdfkit');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, '.')));
 
-app.use(express.static('public'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/generate-bilty', (req, res) => {
     const biltyData = req.body;
@@ -21,7 +25,7 @@ app.post('/generate-bilty', (req, res) => {
         res.writeHead(200, {
             'Content-Length': Buffer.byteLength(pdfData),
             'Content-Type': 'application/pdf',
-            'Content-Disposition': 'attachment;filename=bilty.pdf',
+            'Content-Disposition': 'attachment; filename=bilty.pdf',
         }).end(pdfData);
     });
 
